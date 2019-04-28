fc2b84da813615b88c4755fa720200456d23ebc0
zhyang98 <49853737+zhyang98@users.noreply.github.com>
2019-04-28 16:00:58 -0700

    Add files via upload

---
+++ part2/Assets/MousePosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    Renderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+
+        render.material.shader = Shader.Find("Custom/Edge");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render.material.SetFloat("_Mix", Input.mousePosition.x);
+        render.material.SetFloat("_LookUpDistance", Input.mousePosition.y * 10);
+
+
+        //Debug.Log(Input.mousePosition);
+    }
+}
